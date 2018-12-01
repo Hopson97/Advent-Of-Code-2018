2d1b76c60d2b6a054f625338860e02ee7cf1a6f8
Hopson97 <mhopson@hotmail.co.uk>
2018-12-01 11:35:27 +0000

    Complete aoc day 1 part 2

--- day1.cpp
+++ day1.cpp
@@ -2,8 +2,10 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <unordered_set>
 
-int main() {
+void part1() {
     int freq = 0;
     std::fstream inFile("day1inp.txt");
     std::string line;
@@ -20,5 +22,42 @@ int main() {
         }
     }
     std::cout << freq << '\n';
+}
+
+void part2() {
+    int freq = 0;
+    std::fstream inFile("day1inp.txt");
+    std::string line;
+    std::vector<int> changes;
+    changes.reserve(1024);
+    while(std::getline(inFile, line)) {
+        switch(line[0]) {
+            case '+':
+                changes.push_back(std::stoi(line.substr(1, line.length() - 1)));
+                break;
+            case '-':
+                changes.push_back(-std::stoi(line.substr(1, line.length() - 1)));
+                break;
+            default:
+                break;                
+        }
+    }
+    std::unordered_set<int> newFreqs;
+    newFreqs.reserve(1024 * 64); //reverse the heck out of my memory
+
+    while (true) {
+        for (auto c : changes) {
+            freq += c;
+            if (newFreqs.find(freq) != newFreqs.end()) {
+                std::cout << freq << '\n';
+                return;
+            }
+            newFreqs.insert(freq);
+        }
+    }
+}
+
+int main() {
+    part2();
 }
 
