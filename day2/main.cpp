1616ed3de6916de784e8ca18fd0f1227b1535bac
Hopson97 <mhopson@hotmail.co.uk>
2018-12-02 08:26:58 +0000

    Part 2 complete

--- day2/main.cpp
+++ day2/main.cpp
@@ -30,9 +30,46 @@ void partOne() {
     std::cout << twice * thrice << '\n';
 }
 
+void part2() {
+    std::ifstream inFile("input.txt");
+    std::string line;
+    std::vector<std::string> lines;
+    lines.reserve(256);
+    while (std::getline(inFile, line)) {
+        lines.push_back(line);
+    }
+    const auto length = line.length();
+    const auto lineCount  = lines.size();
+    int indexA = 0;
+    int indexB = 0;
+    [&]{
+    for (size_t oi = 0; oi < lineCount; oi++) {
+        for (size_t ii = 0; ii < lineCount; ii++) {
+            if (ii == oi) continue;
+            int diff = 0;
+            for (size_t i = 0; i < length; i++) {
+                diff += (lines[oi][i] != lines[ii][i]);
+            }
+            if (diff == 1) {
+                indexA = oi;
+                indexB = ii; 
+                return;
+            }
+        }
+    }}();
+    std::string output;
+    for (size_t i = 0; i < length; i++) {
+        if (lines[indexA][i] == lines[indexB][i]) {
+            output.push_back(lines[indexA][i]);
+        }
+    }
+    std::cout << output << '\n';
+}
+
 int main() {
+    
     benchmark(100, []{
         partOne();
+        part2();
     });
-    //partOne();
 }
