37dde2b764649fd11f27218b7a1fe72295ccf882
Hopson97 <mhopson@hotmail.co.uk>
2019-08-24 14:12:09 +0100

    Day7

--- day7/main.cpp
+++ day7/main.cpp
@@ -1,10 +1,63 @@
 #include <fstream>
 #include <iostream>
-#include <unordered_map>
+#include <set>
+#include <vector>
+#include <list>
 #include "../Benchmark.h"
 
+
+
+using Vertices = std::array<std::pair<int, std::vector<int>>, 26>;
+
+struct Graph {
+    std::set<int> startPoints;
+    Vertices vertices;
+};
+
+Graph readFile() {
+    std::ifstream inFile("input.txt");
+    std::string line;
+    Vertices vertices;
+    std::set<int> beginFinder;
+    for (size_t i = 0; i < 26; i++) {
+        vertices[i].first = i;
+        beginFinder.insert(i);
+    }
+
+    while (std::getline(inFile, line)) {
+        int begin = line[5] - 'A';
+        int end   = line[36] - 'A';
+        
+        vertices[begin].second.push_back(end);
+        if (beginFinder.find(end) != beginFinder.end()) {
+            beginFinder.erase(end);
+        }
+    }
+    for (auto& edge : vertices) {
+        std::sort(edge.second.begin(), edge.second.end());
+    }
+
+    return {
+        beginFinder,
+        vertices
+    };
+}
+
 void partOne() {
-    
+    auto graph = readFile();
+    std::string result; result.reserve(32);
+    auto current = graph.startPoints.begin();
+    std::list<int> queue;
+    std::array<bool, 26> visited; visited.fill(false);
+
+    queue.push_back(*graph.startPoints.begin());
+    for (auto& v : graph.vertices) {
+        std::cout << v.first << " -> ";
+        for (auto vert : v.second) {
+            std::cout << vert << " ";
+        }
+        std::cout << '\n';
+    }
 }
 
 void partTwo() {
@@ -12,13 +65,5 @@ void partTwo() {
 }
 
 int main() {
-    Benchmark<100> p1bm("Part 1", &partOne);
-    Benchmark<100> p2bm("Part 2", &partTwo);
-    Benchmark<100> bbm("Part 1 + Part 2", []{
-        partOne(); 
-        partTwo();
-    });
-    p1bm.outputTimes();
-    p2bm.outputTimes();
-    bbm .outputTimes();
+    Benchmark<1>("Part 1", &partOne).outputTimes();
 }
