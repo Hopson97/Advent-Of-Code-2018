e75fc19b3a0fd09b2971a9edc862cd4fcee47e1c
Hopson97 <mhopson@hotmail.co.uk>
2019-10-28 23:00:27 +0000

    Begin work on second efacotr

--- include/aoc/aoc.h
+++ include/aoc/aoc.h
@@ -2,41 +2,11 @@
 
 #include <iostream>
 
-
-
 template <typename Out>
-void output(int year, int day, int part)
+void output(bool output, int year, int day, int part, Out out)
 {
-#if 1
-    std::cout << "Result for " << year << " Day " << day << " Part" << part << std::endl;
-    std::cout << out << '\n' << std::endl;
-#endif
+    if (output) {
+        std::cout << "Result for " << year << " Day " << day << " Part" << part << std::endl;
+        std::cout << out << '\n' << std::endl;
+    }
 }
-/*
-template<int Year, int Day>
-class AdventOfCode
-{
-    public:
-        void setPrint(bool doesPrint) {
-            m_printResults = doesPrint;
-        }
-
-        void title() {
-            std::cout << Year << " Day " << Day << std::endl;
-        }
-
-        virtual void partOne() = 0;
-        virtual void partTwo() = 0;
-    
-    protected:
-        template <typename Out>
-        void output(int part, Out out) {
-            if (m_printResults) {
-                std::cout << "Result for " << Year << " Day " << Day << " Part" << part << std::endl;
-                std::cout << out << '\n' << std::endl;
-            }
-        }
-
-    private:
-        bool m_printResults = true;
-};*/
