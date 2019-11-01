20c6c8a643e8309089acc1fbd0388e324dec104b
Hopson97 <mhopson@hotmail.co.uk>
2019-11-01 13:09:06 +0000

    2017 day 4

--- cpp/2017/day4.cpp
+++ cpp/2017/day4.cpp
@@ -10,30 +10,29 @@
 
 namespace {
     const char *INPUT_PATH = "2017/inputs/day4.txt";
-
-    bool isValidPhrase(const std::string& phrase)
-    {
-        auto words = aoc::string::split(phrase);
-        for (unsigned i = 0; i < words.size(); i++) {
-            for (unsigned j = 0; j < words.size(); j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (words[i] == words[j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 } // namespace
 
 namespace aoc2017 {
     void day4PartOne(bool doPrint)
     {
         auto phrases = aoc::file_io::readAsLines(INPUT_PATH);
-        int validCount = std::count_if(phrases.cbegin(), phrases.cend(), isValidPhrase);
-        aoc::output(doPrint, 2017, 4, 1, validCount);
+        aoc::output(
+            doPrint, 2017, 4, 1,
+            std::count_if(phrases.cbegin(), phrases.cend(),
+                          [](const auto &phrase) {
+                              auto words = aoc::string::split(phrase);
+                              for (unsigned i = 0; i < words.size(); i++) {
+                                  for (unsigned j = 0; j < words.size(); j++) {
+                                      if (i == j) {
+                                          continue;
+                                      }
+                                      if (words[i] == words[j]) {
+                                          return false;
+                                      }
+                                  }
+                              }
+                              return true;
+                          }));
     }
 
     void day4PartTwo(bool doPrint)
