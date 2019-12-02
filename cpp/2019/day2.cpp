1420928e3f378792a1512cacc606adbe5cc972fc
Hopson97 <mhopson@hotmail.co.uk>
2019-12-02 12:46:34 +0000

    2019 d2

--- cpp/2019/day2.cpp
+++ cpp/2019/day2.cpp
@@ -9,48 +9,67 @@
 
 namespace {
     const char *INPUT_PATH = "2019/inputs/day2.txt";
-} // namespace
 
-namespace aoc2019 {
-    void day2PartOne(bool doPrint)
+    void run(std::vector<int>& ops)
     {
-        auto input = aoc::file_io::readAsLine(INPUT_PATH);
-        //auto opstrings = aoc::string::split(input, ',');
-
-        std::vector<int> ops;
-/*
-        std::transform(opstrings.begin(), opstrings.end(), std::back_inserter(ops),
-                       [](auto a) { return std::stoi(a); });
-
-        for (int i = 0; i < (int)opstrings.size(); i += 4) {
+        for (int i = 0; i < (int)ops.size(); i += 4) {
             auto op = ops[i];
             switch (op) {
                 case 1:
-                    ops[i + 3] = ops[i + 1] + ops[i + 2];
+                    ops[ops[i + 3]] = ops[ops[i + 1]] + ops[ops[i + 2]];
                     break;
 
                 case 2:
-                    ops[i + 3] = ops[i + 1] * ops[i + 2];
+                    ops[ops[i + 3]] = ops[ops[i + 1]] * ops[ops[i + 2]];
                     break;
 
                 case 99:
-                    goto end;
-                    break;
+                    return;
 
                 default:
                     std::cout << "ERROR" << op << std::endl;
                     exit(-1);
             }
         }
+    }
+} // namespace
 
-    end:
-*/
+namespace aoc2019 {
+    void day2PartOne(bool doPrint)
+    {
+        auto input = aoc::file_io::readAsLine(INPUT_PATH);
+        auto opstrings = aoc::string::split(input, ',');
+        std::vector<int> ops;
+        std::transform(opstrings.begin(), opstrings.end(), std::back_inserter(ops),
+                       [](auto a) { return std::stoi(a); });
+        ops[1] = 12;
+        ops[2] = 2;
+        run(ops);
+
         aoc::output(doPrint, 2019, 2, 1, ops[0]);
     }
 
     void day2PartTwo(bool doPrint)
     {
-        std::ifstream inFile(INPUT_PATH);
-        aoc::output(doPrint, 2019, 2, 2, "None");
+        auto input = aoc::file_io::readAsLine(INPUT_PATH);
+        auto opstrings = aoc::string::split(input, ',');
+        std::vector<int> ops;
+        std::transform(opstrings.begin(), opstrings.end(), std::back_inserter(ops),
+                       [](auto a) { return std::stoi(a); });
+        auto reset = ops;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                ops[1] = i;
+                ops[2] = j;
+                run(ops);
+                if (ops[0] == 19690720) {
+                    aoc::output(doPrint, 2019, 2, 2, (100 * i + j));
+                    return;
+                }
+                else {
+                    ops = reset;
+                }
+            }
+        }
     }
 } // namespace aoc2019
--- cpp/include/aoc/aoc.h
+++ cpp/include/aoc/aoc.h
@@ -11,4 +11,13 @@ namespace aoc {
             std::cout << out << '\n' << std::endl;
         }
     }
+
+    template <typename Out>
+    void output(bool output, int year, int day, int part, Out out, Out out2)
+    {
+        if (output) {
+            std::cout << "Result for " << year << " Day " << day << " Part" << part << std::endl;
+            std::cout << out << " " << out2 << '\n' << std::endl;
+        }
+    }
 }
