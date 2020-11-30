fb03b9e787697ea79309899b5e3db72ae0f130b0
Hopson97 <mhopson@hotmail.co.uk>
2020-11-30 22:44:55 +0000

    2019 day 1 completed

--- go/2019/day1.go
+++ go/2019/day1.go
@@ -6,7 +6,20 @@ import (
 	"strconv"
 )
 
+func fuelNeeded(mass int) int {
+	return mass/3 - 2
+}
+
+func fueler(in int) int {
+	f := fuelNeeded(in)
+	if f <= 0 {
+		return in
+	}
+	return in + fueler(f)
+}
+
 func main() {
+	//Input
 	buff, err := ioutil.ReadFile("input/day1.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -14,14 +27,25 @@ func main() {
 
 	masses := split(string(buff))
 
+	// Part 1
 	sum := 0
 	for _, mass := range masses {
 		massNumber, err := strconv.Atoi(mass)
 		if err != nil {
 			fmt.Println(err)
 		}
-		sum += massNumber/3 - 2
+		sum += fuelNeeded(massNumber)
 	}
 	fmt.Println(sum)
 
+	// Part 2
+	sum = 0
+	for _, mass := range masses {
+		massNumber, err := strconv.Atoi(mass)
+		if err != nil {
+			fmt.Println(err)
+		}
+		sum += fueler(fuelNeeded(massNumber))
+	}
+	fmt.Println(sum)
 }
