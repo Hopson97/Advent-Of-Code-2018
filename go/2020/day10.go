9948fc6dcaee8ee3dd13576c851ccd7a196e321d
Hopson97 <mhopson@hotmail.co.uk>
2020-12-18 23:59:48 +0000

    d10

--- go/2020/day10.go
+++ go/2020/day10.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func day10() {
-	answers := readFileToString("input/day10.txt")
-	fmt.Println(answers)
-	fmt.Println()
+	joltages := readFileLinesToIntList("input/day10.txt")
+	sort.Ints(joltages)
+	fmt.Println(joltages)
+
+	oneDiff := 1
+	threeDiff := 1
+	for i, v := range joltages {
+		if i + 1 < len(joltages) {
+			diff := joltages[i + 1] - v 
+			if diff == 1 {
+				oneDiff++
+			} else if diff == 3 {
+				threeDiff++
+			}
+		}
+	}
+	fmt.Println(oneDiff, threeDiff, oneDiff * threeDiff)
 }
